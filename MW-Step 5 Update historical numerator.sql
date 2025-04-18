6fc6b2b9f4b082e50f3ae6274913578c1df76f47
WMmelodyliu <102834519+WMmelodyliu@users.noreply.github.com>
2025-04-17 19:07:40 -0700

    Update MW-Step 5 Update historical numerator.sql

--- MW-Step 5 Update historical numerator.sql
+++ MW-Step 5 Update historical numerator.sql
@@ -1,8 +1,10 @@
 -- 1st step (check the latest month)
+/*
 select WM_YEAR_NBR, WM_MONTH_NBR, sum(wm_sales)
 from wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY26Feb
 group by 1, 2
 order by 1, 2
+*/
 
 --2nd FY26Feb and historical data
 CREATE OR REPLACE TABLE wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY26Feb_Total --update
@@ -25,9 +27,10 @@ SELECT *
 FROM wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY26Feb --update
 ORDER BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13;
        
-
+/*
 --3rd
 select WM_YEAR_NBR, WM_MONTH_NBR, sum(wm_sales)
 from wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY26Feb_Total
 group by 1, 2
 order by 1, 2
+*/
