83bc2c3076926aa46e53af0637ca8b8e7023d14f
WMmelodyliu <102834519+WMmelodyliu@users.noreply.github.com>
2025-04-17 19:50:20 -0700

    Update MW-Step 5 Update historical numerator.sql

--- MW-Step 5 Update historical numerator.sql
+++ MW-Step 5 Update historical numerator.sql
@@ -7,26 +7,20 @@ order by 1, 2
 */
 
 --2nd FY26Feb and historical data
-CREATE OR REPLACE TABLE wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY26Feb_Total --update
-AS 
-SELECT *
-FROM wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY25Jan_Total --update with last month
-WHERE (WM_YEAR_NBR, WM_MONTH_NBR) NOT IN (
-    SELECT WM_YEAR_NBR, WM_MONTH_NBR
-    FROM (
-        SELECT distinct WM_YEAR_NBR, WM_MONTH_NBR
-        FROM wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY25Jan_Total
-        ORDER BY WM_YEAR_NBR DESC, WM_MONTH_NBR DESC
-        LIMIT 3
-    )
-)
+create or replace table wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY26Feb_Total --update
+as 
+select *
+from wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY25Jan_Total --update with last month
+where (WM_YEAR_NBR, WM_MONTH_NBR) != (2024,10)
+and (WM_YEAR_NBR, WM_MONTH_NBR) != (2024,11)
+and (WM_YEAR_NBR, WM_MONTH_NBR) != (2024, 12)
+and (WM_YEAR_NBR, WM_MONTH_NBR) != (2025, 1)-- easy to replace
+union distinct
+select *
+from wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY26Feb  --update
+order by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13;
 
-UNION DISTINCT
 
-SELECT *
-FROM wmt-mint-mmr-mw-prod.mw_numerator_dev.mw_numerator_FY26Feb --update
-ORDER BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13;
-       
 /*
 --3rd
 select WM_YEAR_NBR, WM_MONTH_NBR, sum(wm_sales)
